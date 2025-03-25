e50bfa5b5e1a7ac3db39f2bbc4417bbf4845bdeb
Kaiyumari15 <callumstaylor.2806@gmail.com>
2025-03-25 08:59:32 +0000

    Add an API endpoint for deleting ToDoTasks, Add a unit test for this endpoint

--- src/api/todotask.rs
+++ src/api/todotask.rs
@@ -1,5 +1,5 @@
-use rocket::{post, serde::json::Json};
-use crate::database::todotask::create_task;
+use rocket::{post, patch, delete, serde::json::Json};
+use crate::database::todotask::{create_task, delete_task_by_id};
 use crate::model::todotask::ToDoTask;
 use super::Response;
 
@@ -8,7 +8,6 @@ pub async fn create_task_handler(
     input_task: Json<ToDoTask>
 ) -> Response<Json<ToDoTask>> {
     let input_task = input_task.into_inner(); // Deserialise the input from JSON
-    println!("Input task: {:?}", input_task);
 
     // Option<String> -> Option<&str>
     let title = input_task.title.as_deref();
@@ -23,7 +22,6 @@ pub async fn create_task_handler(
 
     // Create the task 
     let created_task = create_task(title, description, completed_at, None).await;
-    println!("Created task: {:?}", created_task);
 
     // Check if there was an error
     if created_task.is_err() {
@@ -39,8 +37,30 @@ pub async fn create_task_handler(
         }
     }
     let task = created_task.unwrap();
-    println!("Task created: {:?}", task);
 
     // Return the response
     Response::Created(Json(task))
+}
+
+#[delete("/tasks/<task_id>")]
+pub async fn delete_task_handler(task_id: &str) -> super::Response<Json<ToDoTask>> {
+    // Delete the task
+    let deleted_task = delete_task_by_id(task_id).await;
+
+    // If there was an error handle it correctly
+    if deleted_task.is_err() {
+        let err = deleted_task.unwrap_err();
+        return match err {
+            crate::database::DBReadError::NotFound(_) => Response::NotFound("Task not found".to_string()),
+            crate::database::DBReadError::Permissions(_) => Response::Forbidden("You do not have permission to delete this task".to_string()),
+            crate::database::DBReadError::Other(_) => {
+                dbg!("Unhandled/Unkown error deleting task: {:?}", err);
+                Response::InternalServerError("There was an unkown error".to_string())
+            }
+        }
+    }
+    let task = deleted_task.unwrap();
+
+    // If the task was deleted, return a 200 OK response with the task
+    Response::Ok(Json(task))
 }
--- src/tests/api/todotask.rs
+++ src/tests/api/todotask.rs
@@ -1,5 +1,3 @@
-
-
 #[cfg(test)]
 mod creating_tasks {
     use crate::model::todotask::ToDoTask;
@@ -23,5 +21,35 @@ mod creating_tasks {
         };
         let response = client.post("/tasks").header(ContentType::JSON).body(serde_json::to_string(&task).unwrap()).dispatch().await;
         assert_eq!(response.status(), Status::Created);
+    }
+}
+
+mod deleting_tasks {
+    use rocket::{http::{ContentType, Status}, local::asynchronous::Client, response, routes};
+
+    use crate::{api::todotask::{create_task_handler, delete_task_handler}, database::connect, model::todotask::ToDoTask};
+
+    #[tokio::test]
+    pub async fn test_delete_task_handler_success() {
+        // set up the database connection and rocket instance
+        let _ = connect().await;
+        let client = Client::tracked(rocket::build().mount("/", routes![create_task_handler, delete_task_handler])).await.unwrap();
+
+        // create a task to delete
+        let task = ToDoTask {
+            id: None,
+            title: Some("Test Task".to_string()),
+            description: Some("This is a test task".to_string()),
+            completed_at: None,
+            created_at: None,
+        };
+        let response = client.post("/tasks").header(ContentType::JSON).body(serde_json::to_string(&task).unwrap()).dispatch().await;
+        assert_eq!(response.status(), Status::Created);
+        let created_task: ToDoTask = response.into_json().await.unwrap(); // get the created task from the response
+
+        // delete the task
+        let response = client.delete(format!("/tasks/{}", created_task.id.unwrap().id.to_string())).dispatch().await;
+        assert_eq!(response.status(), Status::Ok);
+        let deleted_task: ToDoTask = response.into_json().await.unwrap(); // get the deleted task from the response
     }
 }
