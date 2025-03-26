ef1ba959ae066607a020950570e16401f5611818
Kaiyumari15 <callumstaylor.2806@gmail.com>
2025-03-26 10:13:19 +0000

    Add an API andpoint for editing ToDoTasks, Add a unit test for this API endpoint

--- src/api/todotask.rs
+++ src/api/todotask.rs
@@ -1,5 +1,6 @@
+use chrono::format;
 use rocket::{post, patch, delete, serde::json::Json};
-use crate::database::todotask::{create_task, delete_task_by_id};
+use crate::database::todotask::{create_task, delete_task_by_id, edit_task_by_id};
 use crate::model::todotask::ToDoTask;
 use super::Response;
 
@@ -40,6 +41,40 @@ pub async fn create_task_handler(
 
     // Return the response
     Response::Created(Json(task))
+}
+
+#[patch("/tasks/<task_id>", data="<update_task>")]
+pub async fn update_task_handler(task_id: &str, update_task: Json<ToDoTask>) -> super::Response<Json<ToDoTask>> {
+
+    // Deserialise the input from JSON
+    let update_task = update_task.into_inner();
+
+    // Option<String> -> Option<&str>
+    let title = update_task.title.as_deref();
+    let description = update_task.description.as_deref();
+    let completed_at = update_task.completed_at.as_deref();
+
+    // Update the task in the DB
+    let updated_task = edit_task_by_id(task_id, title, description, completed_at).await;
+
+    // If there was an error handle it
+    if updated_task.is_err() {
+        let err = updated_task.unwrap_err();
+        return match err {
+            crate::database::DBEditError::Permissions(_) => Response::Forbidden("You do not have permission to edit this ToDoTask".to_string()),
+            crate::database::DBEditError::NotFound(_) => Response::BadRequest("ToDoItem not found".to_string()),
+            crate::database::DBEditError::BadData(wrapped_err) => Response::BadRequest(format!("Invalid data: {}", wrapped_err).to_string()),
+            crate::database::DBEditError::Other(wrapped_err) => { // If the error is unkown log it and return Status 500
+                dbg!("Unkown/Unhandled error when updating a task: {:?}", wrapped_err);
+                Response::InternalServerError("There was an unkown error".to_string())
+            },
+        }
+    }
+    let task = updated_task.unwrap();
+
+    // Return the edited task
+    Response::Ok(Json(task))
+    
 }
 
 #[delete("/tasks/<task_id>")]
--- src/tests/api/todotask.rs
+++ src/tests/api/todotask.rs
@@ -24,13 +24,59 @@ mod creating_tasks {
     }
 }
 
+#[cfg(test)]
+mod editing_tasks {
+    use chrono::{DurationRound, TimeDelta, Utc};
+    use rocket::{http::{hyper::header::CONTENT_TYPE, ContentType, Status}, local::asynchronous::Client, routes};
+
+    use crate::{api::todotask::{create_task_handler, update_task_handler}, database::connect, model::todotask::ToDoTask};
+
+
+    #[tokio::test]
+    async fn test_update_task_handler_success() {
+        // Set up the database connection and rocket instandce
+        let _ = connect().await;
+        let client = Client::tracked((rocket::build().mount("/", routes![create_task_handler, update_task_handler]))).await.unwrap();
+
+        // Create a task to edit 
+        let original_task = ToDoTask {
+            id: None,
+            title: Some("TESToldtitle".to_string()),
+            description: Some("TESTolddescription".to_string()),
+            completed_at: None,
+            created_at: None,
+        };
+        let create_response = client.post("/tasks").header(ContentType::JSON).body(serde_json::to_string(&original_task).unwrap()).dispatch().await;
+        assert_eq!(create_response.status(), Status::Created, "Error creating task to be edited {}", create_response.status());
+        let old_task: ToDoTask = create_response.into_json().await.unwrap();
+
+
+        // Edit the task
+        let now_str = Utc::now()
+            .duration_round(
+                TimeDelta::try_milliseconds(10)
+                .unwrap())
+            .unwrap()
+            .to_rfc3339_opts(chrono::SecondsFormat::Millis, true);
+        let updated_task = ToDoTask {
+            id: None,
+            title: Some("TESTnewtitle".to_string()),
+            description: Some("TESTnewdescription".to_string()),
+            completed_at: Some(now_str),
+            created_at: None,
+        };
+        let edit_response = client.patch(format!("/tasks/{}", old_task.id.unwrap().id.to_string())).header(ContentType::JSON).body(serde_json::to_string(&updated_task).unwrap()).dispatch().await;
+        assert_eq!(edit_response.status(), Status::Ok, "Error when updating the task: {}: {}", edit_response.status(), edit_response.into_string().await.unwrap());
+    }
+}
+
 mod deleting_tasks {
     use rocket::{http::{ContentType, Status}, local::asynchronous::Client, response, routes};
 
     use crate::{api::todotask::{create_task_handler, delete_task_handler}, database::connect, model::todotask::ToDoTask};
 
     #[tokio::test]
-    pub async fn test_delete_task_handler_success() {
+    async fn test_delete_task_handler_success() {
         // set up the database connection and rocket instance
         let _ = connect().await;
         let client = Client::tracked(rocket::build().mount("/", routes![create_task_handler, delete_task_handler])).await.unwrap();
