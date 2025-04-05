7525a6854f3bc75fac94d2847e8021205625d2b0
Kaiyumari15 <callumstaylor.2806@gmail.com>
2025-04-05 10:31:15 +0100

    Add a function to clear everything beginning with 'TEST' in the database (for unit tests)

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -21,13 +21,6 @@ pub async fn connect() -> () {
 
 pub async fn create_all() -> () {
     let mut response = DB.query("
-    DEFINE TABLE ToDoTask SCHEMAFULL; 
-    DEFINE FIELD title ON TABLE ToDoTask TYPE string;
-    DEFINE FIELD description ON TABLE ToDoTask TYPE option<string>;
-    DEFINE FIELD owner ON TABLE ToDoTask TYPE record<User>;
-    DEFINE FIELD completed_at ON TABLE ToDoTask TYPE option<datetime>;
-    DEFINE FIELD created_at ON TABLE ToDoTask TYPE datetime DEFAULT time::now();
-
     DEFINE TABLE User SCHEMAFULL;
     DEFINE FIELD username ON TABLE User TYPE string;
     DEFINE FIELD email ON TABLE User TYPE string;
@@ -35,6 +28,14 @@ pub async fn create_all() -> () {
     DEFINE FIELD created_at ON TABLE User TYPE datetime DEFAULT time::now();
     DEFINE INDEX uniqueUsername ON TABLE User COLUMNS username UNIQUE;
     DEFINE INDEX uniqueEmail ON TABLE User COLUMNS email UNIQUE;
+
+    DEFINE TABLE ToDoTask SCHEMAFULL; 
+    DEFINE FIELD title ON TABLE ToDoTask TYPE string;
+    DEFINE FIELD description ON TABLE ToDoTask TYPE option<string>;
+    DEFINE FIELD owner ON TABLE ToDoTask TYPE record<User>;
+    DEFINE FIELD completed_at ON TABLE ToDoTask TYPE option<datetime>;
+    DEFINE FIELD created_at ON TABLE ToDoTask TYPE datetime DEFAULT time::now();
+
     ")
     .await
     .expect("Failed to create table ToDoTask and fields"); // Its okay for this function to panic as it is only used when setting up the database or during testing, not during production
@@ -45,6 +46,14 @@ pub async fn create_all() -> () {
     } else {
         panic!("Failed to create table and fields: {:?}", result);
     }
+}
+
+/// Used to clear everything in the database prefixed with 'TEST'
+/// Should only be used in unit tests to prevent conflicts
+pub async fn clear_all_test() -> () {
+    let sql = "
+    DELETE * FROM User WHERE username STARTSWTIH \"TEST\";
+    DELETE * FROM ToDoTask WHERE title STARTSWITH \"TEST\";";
 }
 
 #[derive(Debug, Clone)]
